3cb5bb7f835571d5a0aa8d3197473fc81e263fd0
Alastair Tooth <alastairtooth@gmail.com>
2021-12-19 11:44:07 +1100

    added client folder

---
+++ client/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+      clifford: '#da373d',
+      pall1: '#000957',
+      pall2: '#344CB7',
+      pall3: '#577BC1',
+      hlight: '#EBE645',
+    }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
