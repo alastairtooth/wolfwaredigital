f7c3581c7501ec5f0f4dc92b0658d7f08ab315df
Alastair Tooth <alastairtooth@gmail.com>
2021-12-31 20:40:46 +1100

    build file created

--- server/index.js
+++ server/index.js
@@ -1,16 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require("path");
+const app = express();
 
 const cors = require('cors')
 
 app.use(cors())
 
-app.get('/', (req,res) => {
-  res.send('hello world')
-});
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/design", (req, res) => {
-  res.send("Design Page");
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(3001, () =>{
